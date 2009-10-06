b7cdface92b20e3ab868f385a02153db02406eef
wpc <alex.hal9000@gmail.com>
2009-10-06 14:29:22 +0800

    a little refactoring

--- app/models/preference.js
+++ app/models/preference.js
@@ -1,16 +1,18 @@
-Preference = function() {  
-  return {
+Preference = function() {
+  var public = {
     host: function(v) { return attr('host', v, this) },
     project_id: function(v) { return attr('project_id', v, this) },
     username: function(v) { return attr('username', v, this) },
     password: function(v) { return attr('password', v, this) },
     
     base_auth_token: function() {
-      return make_base_auth(this.username_attr, this.password_attr)
+      return make_base_auth(public.username(), public.password())
     },
     
     api_url: function(){
-      return this.host_attr + "/api/v2/projects/" + this.project_id_attr + "/murmurs.xml"
+      return public.host() + "/api/v2/projects/" + public.project_id() + "/murmurs.xml"
     }
   }
+  
+  return public
 }()
