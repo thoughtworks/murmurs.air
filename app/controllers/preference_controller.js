2a065fdca858622c2326a7ef52398ccaaafd6959
wpc <alex.hal9000@gmail.com>
2009-10-11 22:28:26 +0800

    more refactoring, i am on roll

--- app/controllers/preference_controller.js
+++ app/controllers/preference_controller.js
@@ -1,7 +1,19 @@
 PreferenceController = function() {
+  var preference = new Preference()  
+  
   var public =  {
     open: function() {
       window.open("/app/views/preference.html", "Preference").resizeTo(400, 250)
+    },
+    
+    init: function() {    
+      $('input').each(function(){
+        this.value = preference[this.id]()
+      })
+      
+      $('input').bind('change', function(){
+        preference[this.id](this.value)
+      })
     }
   }
   
