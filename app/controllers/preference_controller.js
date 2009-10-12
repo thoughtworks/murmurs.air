f9cc26be25568552fc1a37877a35ef59a3c9835a
wpc <alex.hal9000@gmail.com>
2009-10-13 00:11:27 +0800

    * sync preference attributes before window close

--- app/controllers/preference_controller.js
+++ app/controllers/preference_controller.js
@@ -1,19 +1,24 @@
 PreferenceController = function() {
   var preference = new Preference()  
   
+  var change_listener = function() {
+    preference[this.id](this.value)
+  }
+  
   var public =  {
     open: function() {
       window.open("/app/views/preference.html", "Preference").resizeTo(400, 250)
     },
     
     init: function() {    
-      $('input').each(function(){
+      $('input').each(function() {
         this.value = preference[this.id]()
       })
       
-      $('input').bind('change', function(){
-        preference[this.id](this.value)
-      })
+      $('input').change(change_listener)
+      window.nativeWindow.addEventListener(air.Event.DEACTIVATE, function() {
+        $('input').each(change_listener)
+      });
     }
   }
   
