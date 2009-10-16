3569817b64833b5ec9f1bebff8677144d80b62ea
wpc <alex.hal9000@gmail.com>
2009-10-16 21:26:17 +0800

    a low tech shadow around the icon

--- app/views/murmur_view.js
+++ app/views/murmur_view.js
@@ -6,7 +6,7 @@ MurmurView = function(murmur) {
       var content = $('<div class="content"></div>').html(MurmurRenderPipes.render(murmur.content()))
       var created_at = $('<span class="created-at"></span>').text(murmur.created_at())
       var body = $('<div class="body"></div>').append(arrow).append(created_at).append(author).append(content)
-      var icon = $('<div class="icon-wrapper"><img class="icon" src="' + murmur.author().icon_path + '"></img>')
+      var icon = $('<div class="icon-border-outer"><div class="icon-border-inner"><img class="icon" src="' + murmur.author().icon_path + '"></div></img>')
       return $('<div class="murmur"></div>').append(icon).append(body)
     }
   }
