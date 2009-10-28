3c6ee5e27ece95fcb7ba20dc0b3d0195c9c818d0
Phoenix <phoenixtoday@gmail.com>
2009-10-28 18:43:28 +0800

    story 18 (reply) added context menu and menu item reply

--- app/controllers/timeline_controller.js
+++ app/controllers/timeline_controller.js
@@ -1,9 +1,14 @@
 TimelineController = function() {
   var timeline = new Timeline()
   var interval = 30 * 1000
+  var menu = new air.NativeMenu()
   
   var public = {
-    init: function(container) { 
+    init: function(container) {
+      var replyItem = new air.NativeMenuItem("Reply")
+      replyItem.addEventListener(air.Event.SELECT, onReplyCommand);
+      menu.addItem(replyItem)
+      
       timeline.onchange(function(event, murmur) {
         if(event == "prepend") {
           container.prepend(new MurmurView(murmur).render())
@@ -11,15 +16,23 @@ TimelineController = function() {
       })
       
       $("button.post").click(PostController.open)
-      
       public.refresh()
       setInterval(public.refresh, interval)
     },
     
     refresh: function() {
       MurmursService.fetch_since(timeline.latest_id(), timeline.prependAll)
-    }
+    },
+    
+    onContextMenu: function(event){
+      event.preventDefault()
+      menu.display(window.nativeWindow.stage, event.clientX, event.clientY)
+    },
   }
+  
+  var onReplyCommand = function(){
+    PostController.open()
+  },
   
   return public
 }()
