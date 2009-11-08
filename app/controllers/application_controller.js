c80933ba446e7c9fb85111f0d199260dca71018e
wpc <alex.hal9000@gmail.com>
2009-11-08 23:54:50 +0800

    menu refactoring

--- app/controllers/application_controller.js
+++ app/controllers/application_controller.js
@@ -15,30 +15,26 @@ ApplicationController = function() {
     if(!timeline_controller()) return
     timeline_controller().refresh()
   }
+
+  var check_updates = function() {
+    updater.checkNow()
+  }
   
   var setup_auto_updater = function() {
     updater.configurationFile = new air.File("app:/config/update_config.xml")
     updater.isCheckForUpdateVisible = false
     updater.initialize()
 
-    setInterval(public.check_updates, 2 * 60 * 60 * 1000) // check updates every 2 hours
-    setTimeout(public.check_updates, 3 * 1000) // do a first check     
+    setInterval(check_updates, 2 * 60 * 60 * 1000) // check updates every 2 hours
+    setTimeout(check_updates, 3 * 1000) // do a first check
   }
   
   var setup_menu = function() {
-    var targetMenu = air.NativeApplication.nativeApplication.menu; 
-
-    var murMenu = targetMenu.addItem(new air.NativeMenuItem("Operations"));
-    murMenu.submenu = new air.NativeMenu();
-
-    var preference = murMenu.submenu.addItem(new air.NativeMenuItem("Preference..."))
-    preference.addEventListener(air.Event.SELECT, PreferenceController.open)
-
-    var refresh = murMenu.submenu.addItem(new air.NativeMenuItem("Refresh"))
-    refresh.addEventListener(air.Event.SELECT, refresh_timeline)
-
-    var checkupdate = murMenu.submenu.addItem(new air.NativeMenuItem("Check for Updates..."))
-    checkupdate.addEventListener(air.Event.SELECT, public.check_updates)
+    air_add_app_menu("Operatinos", air_menu([
+      ["Preference...", PreferenceController.open],
+      ["Refresh", refresh_timeline],
+      ["Check for Updates...", check_updates]
+    ]))
   }
   
   var public =  {
@@ -53,9 +49,6 @@ ApplicationController = function() {
       }
     }, 
     
-    check_updates: function() {
-      updater.checkNow()
-    },
     
     reload_timeline: function() {
       if(!timeline_window) {
